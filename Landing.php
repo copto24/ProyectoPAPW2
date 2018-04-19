72b65cb69486762b5fc46fc4ddee80aa7afb4a9f
César <rayochb@users.noreply.github.com>
2018-04-18 21:26:36 -0500

    Prueba
    Estoy probando github xD

--- Landing.php
+++ Landing.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 
 	<head>
-		<title>PROYECTO PAPW2</title>
+		<title>PROYECTO Programacion y Apliaciones para la Web 2</title>
 		<meta charset="utf-8"> 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">  
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimum-scale=1">  
