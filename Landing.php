9f92d45c0f47317b89268edcbbca449d10c97759
César <rayochb@hotmail.com>
2018-04-20 10:12:48 -0500

    Actualizacion 3

--- Landing.php
+++ Landing.php
@@ -2,7 +2,7 @@
 <html>
 
 	<head>
-		<title>PROYECTO Programacion y Apliaciones para la Web 2</title>
+		<title>PROYECTO PAPW2</title>
 		<meta charset="utf-8"> 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">  
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minimum-scale=1">  
